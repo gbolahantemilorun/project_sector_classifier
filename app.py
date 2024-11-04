d575165eec7604bfe0a52b838ee44d3463aa3944
gbolahantemilorun <gbolahantemilorun@gmail.com>
2024-11-04 15:19:00 +0000

    Updated app.py script

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pickle
 from flask import Flask, render_template, request
@@ -35,4 +36,4 @@ def predict():
 
 # Run the Flask application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
